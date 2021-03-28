638acd61cc65b8b94ddc597dad761ab12c2b32c6
Maciej Tokarz <maciej.tokarz1@wp.pl>
2021-03-28 18:57:55 +0200

    SPIS-60 | dodanie skryptu do polaczenia sie z baza

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const config = require('../config.js')
+
+app.use(cors());
+app.use(express.json());
+
+const dbConnData = {
+    host: config.mongoHost,
+    port: config.mongoPort,
+    database: config.mongoDb
+};
+
+// Połączenie z bazą MongoDb
+
+const mongoose = require('mongoose');
+
+mongoose
+    .connect(`mongodb://${dbConnData.host}:${dbConnData.port}/${dbConnData.database}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(response => {
+        console.log(`Connected to MongoDB. Database name: "${response.connections[0].name}"`)
+        const port = config.port;
+        const host = config.host;
+        app.listen(port, () => {
+            console.log(`server listening at http://${host}:${port}`);
+        });
+    })
+    .catch(error => console.error('Error connecting to MongoDB', error));
