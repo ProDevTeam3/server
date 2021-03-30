df6746938f7c57c8f80f42a29c7e8d2f7bbba871
kamilbakierzynski <kamilbakierzynski@users.noreply.github.com>
2021-03-30 21:42:38 +0000

    Prettified Code!

--- src/index.js
+++ src/index.js
@@ -1,33 +1,38 @@
 import cors from "cors";
 import express from "express";
 const app = express();
-const config = require('../config.js')
+const config = require("../config.js");
 
 app.use(cors());
 app.use(express.json());
 
 const dbConnData = {
-    host: config.mongoHost,
-    port: config.mongoPort,
-    database: config.mongoDb
+  host: config.mongoHost,
+  port: config.mongoPort,
+  database: config.mongoDb,
 };
 
 // Połączenie z bazą MongoDb
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose
-    .connect(`mongodb://${dbConnData.host}:${dbConnData.port}/${dbConnData.database}`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    })
-    .then(response => {
-        console.log(`Connected to MongoDB. Database name: "${response.connections[0].name}"`)
-        const port = config.port;
-        const host = config.host;
-        app.listen(port, () => {
-            console.log(`server listening at http://${host}:${port}`);
-        });
-    })
-    .catch(error => console.error('Error connecting to MongoDB', error));
+  .connect(
+    `mongodb://${dbConnData.host}:${dbConnData.port}/${dbConnData.database}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .then((response) => {
+    console.log(
+      `Connected to MongoDB. Database name: "${response.connections[0].name}"`
+    );
+    const port = config.port;
+    const host = config.host;
+    app.listen(port, () => {
+      console.log(`server listening at http://${host}:${port}`);
+    });
+  })
+  .catch((error) => console.error("Error connecting to MongoDB", error));
