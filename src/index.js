8e855c5d17be7e489685bc2067fbb789d907e989
Robert <robert.jankowski.kontakt@gmail.com>
2021-03-31 10:16:53 +0200

    SPIS-30 | dodanie obywatela poprawione

--- src/index.js
+++ src/index.js
@@ -1,35 +1,41 @@
 import cors from "cors";
 import express from "express";
+import config from "../config.js";
+import citizen from "./routes/citizen";
+
 const app = express();
-const config = require('../config.js');
-const citizen = require('./routes/citizen');
 
 app.use(cors());
 app.use(express.json());
-app.use('/citizen', citizen);
+app.use("/citizen", citizen);
 
 const dbConnData = {
-    host: config.mongoHost,
-    port: config.mongoPort,
-    database: config.mongoDb
+  host: config.mongoHost,
+  port: config.mongoPort,
+  database: config.mongoDb,
 };
 
 // Połączenie z bazą MongoDb
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose
-    .connect(`mongodb://${dbConnData.host}:${dbConnData.port}/${dbConnData.database}`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    })
-    .then(response => {
-        console.log(`Connected to MongoDB. Database name: "${response.connections[0].name}"`)
-        const port = config.port;
-        const host = config.host;
-        app.listen(port, () => {
-            console.log(`server listening at http://${host}:${port}`);
-        });
-    })
-    .catch(error => console.error('Error connecting to MongoDB', error));
+  .connect(
+    `mongodb://${dbConnData.host}:${dbConnData.port}/${dbConnData.database}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .then((response) => {
+    console.log(
+      `Connected to MongoDB. Database name: "${response.connections[0].name}"`
+    );
+    const port = config.port;
+    const host = config.host;
+    app.listen(port, () => {
+      console.log(`server listening at http://${host}:${port}`);
+    });
+  })
+  .catch((error) => console.error("Error connecting to MongoDB", error));
--- src/routes/citizen.js
+++ src/routes/citizen.js
@@ -1,25 +1,14 @@
-const express = require('express');
-const router = express.Router({mergeParams: true});
-// const Main = require('../models/Main');
+import Express from "express";
+const router = Express.Router({ mergeParams: true });
+const Main = require("../models/Main");
 
-class Main {
-    constructor(args) {
-        this.PESEL = args.PESEL
-    }
-    async save() {
-        return this
-    }
-}
-
-router.post('/addCitizen', async (req, res) => {
-    try {
-      const person = new Main({...req.body})
-      const saved = await person.save()
-      return res.send({savedId: saved.PESEL});
-    }
-    catch(error) {
-      return res.send({errorMessage: error})
-    }
-      
-  });
-module.exports = router;
+router.post("/addCitizen", async (req, res) => {
+  try {
+    const person = new Main(req.body);
+    const saved = await person.save();
+    return res.send({ savedId: saved.PESEL });
+  } catch (error) {
+    return res.send({ errorMessage: error });
+  }
+});
+export default router;
