b5dba608ccb04589c2e7f1a91444e5fa02f64c90
pauljackals <pauljackals@users.noreply.github.com>
2021-04-15 16:15:19 +0000

    Prettified Code!

--- src/routes/stats.js
+++ src/routes/stats.js
@@ -5,40 +5,45 @@ const Main = require("../models/MainModel");
 
 router.get("/industryByNationality", async (req, res) => {
   try {
-    const citizens = await Main.find().populate('contract').populate('company')
+    const citizens = await Main.find().populate("contract").populate("company");
 
     const nationalities = citizens.reduce((citizen, accumulator) => {
+      const nationality = citizen.nationality;
+      const industriesDuplicates = citizen.contract.map(
+        (contractSingle) => contractSingle.company.industry
+      );
+      const industries = industriesDuplicates.filter(
+        (v, i, a) => a.indexOf(v) === i
+      );
 
-      const nationality = citizen.nationality
-      const industriesDuplicates = citizen.contract.map(contractSingle => contractSingle.company.industry)
-      const industries = industriesDuplicates.filter((v, i, a) => a.indexOf(v) === i);
+      const isNationalityPresent = !!accumulator[nationality];
 
-      const isNationalityPresent = !!accumulator[nationality]
-
-      if(isNationalityPresent) {
+      if (isNationalityPresent) {
         return {
           ...accumulator,
           [nationality]: {
             ...accumulator[nationality],
             ...industries.reduce((industry, accumulator2) => {
-              const industryValue = accumulator[nationality][industry]
-              if(industryValue) {
-                return {...accumulator2, [industry]: industryValue+1}
+              const industryValue = accumulator[nationality][industry];
+              if (industryValue) {
+                return { ...accumulator2, [industry]: industryValue + 1 };
               } else {
-                return {...accumulator2, [industry]: 1}
+                return { ...accumulator2, [industry]: 1 };
               }
-            }, {})
-          }
-        }
+            }, {}),
+          },
+        };
       } else {
         return {
           ...accumulator,
-          [nationality]: industries.reduce((industry, accumulator2) => ({...accumulator2, [industry]: 1}), {})
-        }
+          [nationality]: industries.reduce(
+            (industry, accumulator2) => ({ ...accumulator2, [industry]: 1 }),
+            {}
+          ),
+        };
       }
-    }, {})
-    return res.json({nationalities})
-
+    }, {});
+    return res.json({ nationalities });
   } catch (error) {
     return res.json({ errorMessage: error });
   }
